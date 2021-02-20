b06e42b366532ecdca91ec2ba02204b39e9cc3cc
Aca1995 <petrovic.aleksandar5595@gmail.com>
2021-02-21 00:27:57 +0100

    Create signIn form and styles

--- netflix-clone/src/pages/signin.js
+++ netflix-clone/src/pages/signin.js
@@ -1,3 +1,39 @@
+import { useState } from "react";
+import { HeaderContainer } from "../containers/header";
+import { FooterContainer } from "../containers/footer";
+import { Form } from "../components";
+
 export default function Signin() {
-  return <p>Hello from the sign in!</p>;
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [error, setError] = useState("");
+
+  // check form input elements validity
+
+  return (
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign in</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+
+          <Form.Base onSubmit={handleSignIn} method="POST">
+            <Form.Input
+              placeholder="email"
+              value={email}
+              onChange={({ target }) => setEmail(target.value)}
+            />
+            <Form.Input
+              type="password"
+              autoComplete="off"
+              placeholder="password"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </Form.Base>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
+  );
 }
