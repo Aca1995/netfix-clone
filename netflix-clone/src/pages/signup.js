1b004c3e848824918f654f6fad65b7e64e71281c
Aca1995 <petrovic.aleksandar5595@gmail.com>
2021-02-21 13:50:04 +0100

    Create signUp page and connect it with firebase

--- netflix-clone/src/pages/signup.js
+++ netflix-clone/src/pages/signup.js
@@ -1,3 +1,92 @@
+import { useState, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import { FirebaseContext } from "../context/firebase";
+import { HeaderContainer } from "../containers/header";
+import { FooterContainer } from "../containers/footer";
+import { Form } from "../components";
+import * as ROUTES from "../constants/routes";
+
 export default function Signup() {
-  return <p>Hello from the sign up!</p>;
+  const history = useHistory();
+
+  const { firebase } = useContext(FirebaseContext);
+
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  // just a basic check, this can be made better
+  const isInvalid = firstName === "" || password === "" || email === "";
+
+  const handleSignUp = (event) => {
+    event.preventDefault();
+
+    return firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then((result) => {
+        // firebase don't have a function for registering user with first name, so this is the way to do it
+        // after user is created, we will update the profile with the first name and user image
+        result.user
+          .updateProfile({
+            displayName: firstName,
+            photoURL: Math.floor(Math.random() * 5) + 1,
+          })
+          .then(() => {
+            history.push(ROUTES.BROWSE);
+          });
+      })
+      .catch((error) => {
+        setFirstName("");
+        setEmail("");
+        setPassword("");
+        setError(error.message);
+      });
+  };
+
+  // We are using Form reusable component, because it's basically the same as SignIn page
+  return (
+    <>
+      <HeaderContainer>
+        <Form>
+          <Form.Title>Sign Up</Form.Title>
+          {error && <Form.Error>{error}</Form.Error>}
+
+          <Form.Base onSubmit={handleSignUp} method="POST">
+            <Form.Input
+              placeholder="First name"
+              value={firstName}
+              onChange={({ target }) => setFirstName(target.value)}
+            />
+
+            <Form.Input
+              placeholder="Email address"
+              value={email}
+              onChange={({ target }) => setEmail(target.value)}
+            />
+            <Form.Input
+              type="password"
+              value={password}
+              autoComplete="off"
+              placeholder="Password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Form.Submit disabled={isInvalid} type="submit" data-testid="sign-up">
+              Sign Up
+            </Form.Submit>
+          </Form.Base>
+
+          <Form.Text>
+            Already a user? <Form.Link to="/signin">Sign in now.</Form.Link>
+          </Form.Text>
+          <Form.TextSmall>
+            This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn
+            more.
+          </Form.TextSmall>
+        </Form>
+      </HeaderContainer>
+      <FooterContainer />
+    </>
+  );
 }
