127dfe888a634099bfaf4c31160738ea53935c7a
Aca1995 <petrovic.aleksandar5595@gmail.com>
2021-02-21 13:07:39 +0100

    Connect firebase to signIn page

--- netflix-clone/src/pages/signin.js
+++ netflix-clone/src/pages/signin.js
@@ -1,14 +1,40 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { useHistory } from "react-router-dom";
+import { FirebaseContext } from "../context/firebase";
 import { HeaderContainer } from "../containers/header";
 import { FooterContainer } from "../containers/footer";
 import { Form } from "../components";
+import * as ROUTES from "../constants/routes";
 
 export default function Signin() {
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
+  // custom hook
+  const history = useHistory();
+
+  const { firebase } = useContext(FirebaseContext);
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [error, setError] = useState("");
 
   // check form input elements validity
+  const isInvalid = password === "" || email === "";
+
+  const handleSignIn = (event) => {
+    event.preventDefault();
+
+    // firebase work here
+    firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password)
+      .then(() => {
+        history.pushState(ROUTES.BROWSE);
+      })
+      .catch((error) => {
+        setEmail("");
+        setPassword("");
+        setError(error.message);
+      });
+  };
 
   return (
     <>
@@ -30,6 +56,17 @@ export default function Signin() {
               value={password}
               onChange={({ target }) => setPassword(target.value)}
             />
+            <Form.Submit disabled={isInvalid} type="submit">
+              Sign In
+            </Form.Submit>
+
+            <Form.Text>
+              New to Netflix? <Form.Link to="/signup">Sign up now.</Form.Link>
+            </Form.Text>
+            <Form.TextSmall>
+              This page is protected by Google reCAPTCHA to ensure you're not a bot. Learn
+              more.
+            </Form.TextSmall>
           </Form.Base>
         </Form>
       </HeaderContainer>
