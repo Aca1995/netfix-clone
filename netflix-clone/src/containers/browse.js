de6a817f80e1f9f3fc258a90bbff722ff9755a88
Aca1995 <petrovic.aleksandar5595@gmail.com>
2021-02-22 14:32:09 +0100

    Make profile and spinner

--- netflix-clone/src/containers/browse.js
+++ netflix-clone/src/containers/browse.js
@@ -1,3 +1,28 @@
+import { useState, useEffect, useContext } from "react";
+import { SelectProfileContainer } from "./profiles";
+import { FirebaseContext } from "../context/firebase";
+import { Loading } from "../components";
+
 export function BrowseContainer({ slides }) {
-  return <p>Hello from BrowseContainer</p>;
+  const { firebase } = useContext(FirebaseContext);
+  const user = firebase.auth().currentUser || {};
+
+  const [profile, setProfile] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, [profile.displayName]);
+
+  return profile.displayName ? (
+    loading ? (
+      <Loading src={user.photoURL} />
+    ) : (
+      <Loading.ReleaseBody />
+    )
+  ) : (
+    <SelectProfileContainer user={user} setProfile={setProfile} />
+  );
 }
