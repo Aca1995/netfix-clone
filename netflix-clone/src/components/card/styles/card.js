d7c15ad2782eacbfb597cd4ec17d1d8d8d632757
Aca1995 <petrovic.aleksandar5595@gmail.com>
2021-03-04 14:55:01 +0100

    create card component

---
+++ netflix-clone/src/components/card/styles/card.js
@@ -0,0 +1,38 @@
+import styled from "styled-components/macro";
+
+export const Title = styled.p`
+  font-size: 24px;
+  color: #e5e5e5;
+  font-weight: bold;
+  margin-left: 56px;
+  margin-right: 56px;
+  margin-top: 0;
+`;
+
+export const Container = styled.div``;
+
+export const Group = styled.div``;
+
+export const Subtitle = styled.p``;
+
+export const Text = styled.p``;
+
+export const Entities = styled.div``;
+
+export const Meta = styled.div``;
+
+export const Image = styled.img``;
+
+export const Item = styled.div``;
+
+export const FeatureText = styled.p``;
+
+export const Feature = styled.div``;
+
+export const FeatureTitle = styled(Title)``;
+
+export const FeatureClose = styled.button``;
+
+export const Content = styled.div``;
+
+export const Maturity = styled.div``;
